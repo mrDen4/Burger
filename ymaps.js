1f60d9e5c54d56c8584ae08b965b15e68b2e1334
Denis Davydov <den.saint.k@gmail.com>
2018-01-28 23:43:03 +0300

    add yandex maps

---
+++ ymaps.js
@@ -0,0 +1,19 @@
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init() {
+    myMap = new ymaps.Map("map", {
+        center: [55.99750671, 37.21066432],
+        zoom: 13,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    myPlacemark = new ymaps.Placemark([55.996185, 37.211611], {
+        hintContent: 'Cafe friends',
+        balloonContent: 'Cafe friends'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
