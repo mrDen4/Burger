d03c240cb02b89c670c5cdcacec082fcb436486e
Denis Davydov <den.saint.k@gmail.com>
2018-02-05 00:46:55 +0300

    add menuOpen and menuClose

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const openMenu = document.querySelector('#openMenu');
+const menu = document.querySelector('#menu');
+const closeMenu = document.querySelector('#closeMenu');
 
 openMenu.addEventListener('click', function() {
-    console.log('Menu');
+    menu.classList.add('natural__menu--active');
 });
+
+closeMenu.addEventListener('click', function() {
+    menu.classList.remove('natural__menu--active');
+});
+
