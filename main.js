abe41840ca45371fe9211bd5a4f80d78a6a06da1
Denis Davydov <den.saint.k@gmail.com>
2018-02-06 15:01:13 +0300

    add button in header and burger-menu

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const openMenu = document.querySelector('#openMenu');
-const menu = document.querySelector('#menu');
-const closeMenu = document.querySelector('#closeMenu');
+const openMenu = document.querySelector('.header__menu');
+const menu = document.querySelector('.natural__menu');
+const closeMenu = document.querySelector('.menu__exit');
 
 openMenu.addEventListener('click', function() {
     menu.classList.add('natural__menu--active');
+    openMenu.classList.add('header__menu--active');
 });
 
 closeMenu.addEventListener('click', function() {
     menu.classList.remove('natural__menu--active');
+    openMenu.classList.remove('header__menu--active');
 });
 
