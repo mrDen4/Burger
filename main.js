c85ee9181e9b6604799ad862eff7d07065b4dbfe
Denis Davydov <den.saint.k@gmail.com>
2018-02-11 03:33:11 +0300

    fixed burger-menu

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const openMenu = document.querySelector('.header__menu');
 
 openMenu.addEventListener('click', function() {
-    if (menu.classList.contains('natural__menu--active')) {
-        menu.classList.remove('natural__menu--active');
+    if (menu.classList.contains('natural__burger-menu--active')) {
+        menu.classList.remove('natural__burger-menu--active');
         openMenu.classList.remove('header__menu--active')
     }
     else {
-        menu.classList.add('natural__menu--active');
+        menu.classList.add('natural__burger-menu--active');
         openMenu.classList.add('header__menu--active')
     }
 });
