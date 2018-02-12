c2e888acc82e70028b0f2d3abd147c6002603aa4
Denis Davydov <den.saint.k@gmail.com>
2018-02-12 16:08:41 +0300

    Add team accordion

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const openMenu = document.querySelector('.header__menu');
+const menu = document.querySelector('.natural__burger-menu');
+const teamBtn = document.querySelector('.team__item-btn');
+const teamItem = document.querySelector('.team__item');
 
 openMenu.addEventListener('click', function() {
     if (menu.classList.contains('natural__burger-menu--active')) {
@@ -8,6 +11,15 @@ openMenu.addEventListener('click', function() {
     else {
         menu.classList.add('natural__burger-menu--active');
         openMenu.classList.add('header__menu--active')
+    }
+});
+
+teamBtn.addEventListener('click', function() {
+    if (teamItem.classList.contains('team__item--active')) {
+        teamItem.classList.remove('team__item--active')
+    }
+    else {
+        teamItem.classList.add('team__item--active');
     }
 });
 
