2009e291e5f9e9a3ff5064e386e1ebab36109590
Denis Davydov <den.saint.k@gmail.com>
2018-02-18 01:14:39 +0300

    add form

--- js/main.js
+++ js/main.js
@@ -57,4 +57,43 @@ $(document).ready(() => {
         nextText : '',
         prevText: ''
     });
+
+    //Send Forms
+    $('#delivery-form').on('submit', submitForm);
+
+    function submitForm (ev) {
+    ev.preventDefault();
+    
+    var form = $(ev.target),
+    data = form.serialize(),
+    url = form.attr('action'),
+    type = form.attr('method');
+
+    ajaxForm(form).done(function(msg) {
+        var mes = msg.mes,
+            status = msg.status;
+
+            if (status === 'ok') {
+                form.append('<p class="success">' + mes + '</p>');
+            } else{
+                form.append('<p class="error">' + mes + '</p>');
+            }
+    }).fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+}
+
+
+//Общая функция для форм на стр
+    var ajaxForm = function (form) {
+    var data = form.serialize(),
+        url = form.attr('action');
+    
+    return $.ajax({
+        type: 'POST',
+        url: url,
+        dataType : 'JSON',
+        data: data
+    })
+};
 });
---
+++ server.php
@@ -0,0 +1,57 @@
+<?php 
+
+    $name = $_POST['name'];
+    $telephone = $_POST['telephon'];
+    $street = $_POST['street'];
+    $house = $_POST['house'];
+    $housing = $_POST['housing'];
+    $apartment = $_POST['apartment'];
+    $floor = $_POST['floor'];
+    $comment = $_POST['comment'];
+    $buy = $_POST['buy'];
+    
+
+    $disturb = $_POST['disturb']; // 1 or null
+    $disturb = isset($disturb) ? 'НЕТ' : 'ДА';
+    
+
+    $mail_message = '
+    <html>
+    <head>
+        <title>Ответ от сервера</title>
+    </head>
+    <body>
+        <h1>Данные клиента</h1>
+        <ul>
+            <li> Имя: ' . $name . ' </li>
+            <li> Телефон: ' . $telephone . ' </li>
+            <li> Улица : ' . $street . ' </li>
+            <li> Дом : ' . $house . ' </li>
+            <li> Корпус : ' . $housing . ' </li>
+            <li> Квартира : ' . $apartment . ' </li>
+            <li> Этаж : ' . $floor . ' </li>
+            <li> Комментарий : ' . $comment . ' </li>
+            <li> Как оплачиваем? : ' . $buy . ' </li>
+            <li> Не перезванивать? : ' . $disturb . ' </li>
+        </ul>
+    </body>
+    </html>';
+
+    $headers = "From: Администратор сайта <admin@burger.com>\r\n".
+    "MIME-Version: 1.0" . "\r\n" .
+    "Content-type: text/html; charset=UTF-8" . "\r\n";
+
+    $mail = mail('mr.den.saint@yandex.ru', 'Заказ', $mail_message, $headers);
+
+    $data = [];
+
+    if ($mail) {
+        $data['status'] = 'ok';
+        $data['mes'] = 'Заказ принят';
+    }else{
+        $data['status'] = 'no';
+        $data['mes'] = 'Проблемы с формой';
+    }
+
+    echo json_encode($data);
+?>
